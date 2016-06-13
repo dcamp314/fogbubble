789589ace16dda5e1961d32634600b2c8852d34d
Christian Campbell <dcamp@alumni.ufl.edu>
2016-06-12 21:21:19 -0700

    add other windows

--- fogbubble.rb
+++ fogbubble.rb
@@ -8,7 +8,20 @@ begin
   noecho
   curs_set 0
 
-  clk = Window.new(2, cols, lines - 2, 0)
+  windows = []  # keep a list of windows
+
+  # create left-hand side window (resolved cases)
+  windows << lhs = Window.new(lines - 4, (cols - 1) / 2, 0, 0)
+
+  # create right-hand side window (active cases)
+  windows << rhs = Window.new(lines - 4, cols / 2, 0, cols - (cols / 2))
+
+  # create "Working On" window
+  windows << won = Window.new(2, cols, lines - 4, 0)
+  won.attron(A_REVERSE) { won.addstr(" Working On ") }
+
+  # create clock/ticker window
+  windows << clk = Window.new(2, cols, lines - 2, 0)
   fmtClk = "%H:%M:%S"
 
   loop do
@@ -17,7 +30,10 @@ begin
 
     clk.setpos(0, 0)
     clk.attron(A_REVERSE) { clk.addstr(Time.now.strftime(" #{fmtClk} ")) }
-    clk.refresh
+
+    # refresh
+    windows.each {|w| w.noutrefresh }
+    doupdate
 
     # sleep for remainder of current wall clock second
     sleep(1 - 1e-6 * Time.now.usec)
