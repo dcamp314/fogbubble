ee7ea82c47aaae6bea9922f60c1c4e3b9dfc5f11
Christian Campbell <dcamp@alumni.ufl.edu>
2016-06-12 18:41:03 -0700

    main event loop

--- fogbubble.rb
+++ fogbubble.rb
@@ -2,3 +2,16 @@
 
 require 'curses'
 include Curses
+
+begin
+  init_screen
+  noecho
+  curs_set 0
+
+  loop do
+    sleep 1
+  end
+rescue Interrupt  # Ctrl-C to break
+ensure
+  close_screen
+end
