38fb543dae7dad82156ed3c202df36cbee891db8
Christian Campbell <dcamp@alumni.ufl.edu>
2016-06-12 19:09:13 -0700

    create clock window (clk)

--- fogbubble.rb
+++ fogbubble.rb
@@ -8,8 +8,16 @@ begin
   noecho
   curs_set 0
 
+  clk = Window.new(2, cols, lines - 2, 0)
+  fmtClk = "%H:%M:%S"
+
   loop do
-    sleep 1
+    clk.setpos(0, 0)
+    clk.addstr(Time.now.strftime(" #{fmtClk} "))
+    clk.refresh
+
+    # sleep for remainder of current wall clock second
+    sleep(1 - 1e-6 * Time.now.usec)
   end
 rescue Interrupt  # Ctrl-C to break
 ensure
